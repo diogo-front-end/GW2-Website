6ba69c297b0c2252b6a8759336bbc5e354a1c688
diogo-front-end <diogomarquessimoes@gmail.com>
2022-11-20 01:42:03 +0100

    Started working releases page
    -Started working on releases pages
    -Updated index.html
    -Improved hamburger menu
    -made changes on mobile navigation

--- JS/navbar.js
+++ JS/navbar.js
@@ -1,14 +1,21 @@
+const primaryHeader = document.querySelector(".primary-header");
 const primaryNav = document.querySelector(".primary-navigation");
 const navBtnToggle = document.querySelector(".menu-btn");
+
+
 const navbarOpen = "translateX(0%)";
 const navbarClose = null;
+const primaryHeaderColorOpen = "rgba(0, 0, 0 ,1)";
+const primaryHeaderColorClose = "rgba(0, 0, 0, 0.75)"
 
 
 navBtnToggle.addEventListener("click", () => {
     navBtnToggle.classList.toggle('open');
     if (navBtnToggle.classList.contains('open')) {
         primaryNav.style.transform = navbarOpen;
+        primaryHeader.style.backgroundColor = primaryHeaderColorOpen;
     } else {
         primaryNav.style.transform = navbarClose;
+        primaryHeader.style.backgroundColor = primaryHeaderColorClose;
     }
 });
